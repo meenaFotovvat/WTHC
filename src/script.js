005e6a76f522b2bd4306f7c3e64076f256fd90b7
meenaFotovvat <meenafotovvat@gmail.com>
2023-07-30 17:46:31 +0330

    new changes

--- src/script.js
+++ src/script.js
@@ -1,46 +1,52 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-
-function convertToF(centigrade) {
-  let fahrenheit = (centigrade * 9) / 5 + 32;
-  return Math.round(fahrenheit);
-}
-
-function convertToC(fahrenheit){
-  
-}
-// let userCity = prompt("Enter a city?").toLowerCase();
-
-// if (userCity in weather) {
-//   alert(
-//     `It is currently ${Math.round(weather[userCity].temp)}°C (${convertToF(
-//       weather[userCity].temp
-//     )}°F) in ${userCity} with a humidity of ${
-//       weather[userCity].humidity
-//     }%.`
-//   );
-// } else {
-//   alert(
-//     `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${userCity}`
-//   );
+// function submit(event) {
+//   event.preventDefault();
+//   let userCity = document.querySelector("#user-searched");
+//   let city = document.querySelector("#search-input").value;
+//   userCity.innerHTML = city;
 // }
+
+// function formatDate(date) {
+//   let days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
+
+//   return `${days[date.getDay()]} ${date.getHours()}:${date.getMinutes()} PM`;
+// }
+
+// let searchCity = document.querySelector("#search");
+// searchCity.addEventListener("submit", submit);
+// let date = document.querySelector("#date");
+// date.innerHTML = formatDate(new Date());
+
+function convertToFarenheit(event) {
+  let tempValue = document.querySelector("#temp-value").innerHTML;
+  // if (event.target.innerHTML !== "°C") {
+  const farenheit = Math.round((tempValue * 9) / 5 + 32);
+  console.log("farenheit", farenheit);
+  // } else {
+  //   console.log("herectf");
+  //   return;
+  // }
+}
+
+function convertToCentigrade(event) {
+  // if (event.target.innerHTML === "°F") {
+  let tempValue = document.querySelector("#temp-value").innerHTML;
+  const centigrade = Math.round(((tempValue - 32) * 5) / 9);
+  console.log("centigrade", centigrade);
+  // } else {
+  //   console.log("hereftc");
+  //   return;
+  // }
+}
+
+let centigrade = document.querySelector("#centigrade");
+centigrade.addEventListener("click", convertToCentigrade);
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", convertToFarenheit);
